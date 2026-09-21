16ee67fec4820e020b82baf6f0932fc51a19e7ef
kishoreKishore3107 <kishoreninja64@gmail.com>
2026-09-21 20:24:43 +0530

    Create arraylist.java

---
+++ arraylist.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[] args){
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("volvo");
+        cars.add("benz");
+         cars.add("maruthi");
+         cars.add("porche");
+        System.out.println(cars);
+    } 
+}
+
+o/p:
+
+[volvo, benz, maruthi, porche]
