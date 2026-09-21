28ddeeca879fcf919e0316a80495e0e28e7430b9
kishoreKishore3107 <kishoreninja64@gmail.com>
2026-09-21 20:26:08 +0530

    Create list.java

---
+++ list.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        var num = new ArrayList<Integer>();
+        num.add(33);
+        num.add(15);
+         num.add(20);
+         num.add(34); 
+num.add(8);
+num.add(12);
+        Collections.sort(num);
+        for (int i : num){
+System.out.println(i);
+        }    
+    } 
+}
+
+
